8b46a7bbc74d102e945a72ef402df0dd7df5c1b3
abylya <abdyla0303@yandex.ru>
2025-12-06 22:13:16 +0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Test1 from './practi_learn/test1.jsx'
+// import Test1 from './practi_learn/test1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

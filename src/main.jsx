ffc2c5bcb4d2d4f93954b2d5e7b7718d382d99e5
abylya <abdyla0303@yandex.ru>
2025-12-06 21:42:49 +0300

    add pagination

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Test1 from './practi_learn/test1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <Test1></Test1> */}
   </StrictMode>,
 )

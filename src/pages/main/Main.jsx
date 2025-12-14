bdc0612cd646af78facef69ef18047361e74adb8
abylya <abdyla0303@yandex.ru>
2025-12-15 01:15:39 +0300

    add Latest news

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,35 +1,18 @@
 
 import styles from "./styles.module.css";
-import { TOTAL_PAGE, PAGE_SIZE} from "../../constants/constants.js";
-import { getNews } from "../../api/apiNews.js";
-import useFilter from "../../hooks/useRequestFilter.js";
 import NextNewsWrapper from "../../companents/nextNewsWrapper/NextNewsWrapper";
-import useFetch from "../../helps/useFetch.js";
 import LatestNewsWrap from "../../companents/latestNewsWrap/LatestNewsWrap";
 
 
 //const NewsContext=createContext([]);
 export default function Main() {
 
- const {requestParam,chengeRequestParam}=useFilter({
-  page_number:1,
-  page_size:PAGE_SIZE,
-  category:null,
-  keywords:'',
- })
- 
- const {data,loading}=useFetch(getNews,requestParam);
- const currentPage=data?.page;
-
-
-
- 
  //console.log(currentPage); 
  return <main className={styles.main}>
 
-      <LatestNewsWrap news={data&&data.news} loading={loading}></LatestNewsWrap>
+      <LatestNewsWrap></LatestNewsWrap>
 
-      <NextNewsWrapper changePage={chengeRequestParam} currentCategory={requestParam.category}  currentPage={currentPage} news={data?.news} loading={loading}></NextNewsWrapper>
+      <NextNewsWrapper ></NextNewsWrapper>
        
   </main>
   

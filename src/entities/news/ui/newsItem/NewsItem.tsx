0f51fa2fbe010b3031779961d1567881a67bc505
abylya <abdyla0303@yandex.ru>
2026-01-25 22:13:17 +0300

    add newsPage

--- src/entities/news/ui/newsItem/NewsItem.tsx
+++ src/entities/news/ui/newsItem/NewsItem.tsx
@@ -2,16 +2,18 @@ import timeAgo from "@/shared/helps/timeAgo";
 import styles from "./styles.module.css";
 import type { INews, T_type } from "../../index";
 import Image from "@/shared/ui/image/Image";
+import type { ReactNode } from "react";
 interface IProps {
   news: INews;
   type?: T_type;
+  viewNewsSlot?: (news: INews) => ReactNode;
 }
-export default function NewsItem({ news, type = "item" }: IProps) {
+export default function NewsItem({ news, type, viewNewsSlot }: IProps) {
   return (
     <>
       <li
         key={news.id}
-        className={`${styles.item}  ${type === "bunner" && styles.item_bunner}`}
+        className={`${styles.item}  ${type === "bunner" ? styles.item_bunner : styles.item_item}`}
       >
         {type === "bunner" ? (
           <Image image={news?.image}></Image>
@@ -25,6 +27,7 @@ export default function NewsItem({ news, type = "item" }: IProps) {
           <h3 className={styles.title}>{news.title}</h3>
           <p className={styles.extra}>{timeAgo(news.published)}</p>
         </div>
+        {viewNewsSlot ? viewNewsSlot(news) : null}
       </li>
     </>
   );
